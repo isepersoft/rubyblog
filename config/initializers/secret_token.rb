815e0928dc854c8652895729be6679998e6e2562
persoft <isepersoft@gmail.com>
2014-01-26 22:18:52 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyblog::Application.config.secret_token = '0c9fc7ca765603a538a96a95186ba38785d9e0b2d6b7aacd1fde9bf984065bfffc5e84e534e8a3305937520b0bea5f02ae42ba072997a7bae3facc76611262d4'
